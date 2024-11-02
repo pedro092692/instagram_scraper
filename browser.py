c948077434a2354b3c3a510b52c224d5aa3f546f
pedro092692 <pedro_092692@hotmail.com>
2024-11-02 00:22:09 -0400

    add fucntion to download ig images from account

--- browser.py
+++ browser.py
@@ -1,9 +1,11 @@
+import selenium.common.exceptions
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+import requests
 import time
 import os
 
@@ -11,13 +13,15 @@ import os
 class Browser:
 
     def __init__(self, target_account):
-        self.url = 'https://www.instagram.com/'
+        self.url = 'https://www.instagram.com'
         self.account = target_account
 
         # setup selenium
         self.options = webdriver.ChromeOptions()
         self.options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=self.options)
+        self.driver.get('https://www.google.com/')
+
         self.driver.get(self.url)
 
     def login(self):
@@ -33,6 +37,127 @@ class Browser:
         time.sleep(10)
 
     def load_target_account(self):
-
         new_url = f'{self.url}/{self.account}'
         self.driver.get(new_url)
+
+    def load_image_links(self):
+        html = self.driver.find_element(By.TAG_NAME, value='html')
+
+        # find div container of image post.
+        container_1 = '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div'
+        container_2 = '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/div[2]'
+
+        try:
+            image_container = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((
+                By.XPATH, container_1
+            )))
+        except selenium.common.exceptions.TimeoutException:
+            image_container = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((
+                By.XPATH, container_2
+            )))
+
+        try:
+            with open(f'downloads/{self.account}/{self.account}_info_posts.txt', mode='r') as posts_info:
+                last_post = int(posts_info.read())
+                post_number = last_post + 3
+                last_post = post_number
+        except FileNotFoundError:
+            post_number = 4
+            last_post = 0
+
+        image_links = self.get_links(post_number, image_container, html, last_post)
+        return image_links
+
+
+    def get_links(self, post_number, image_container, html, last_post):
+        image_links = []
+        scroll = 0
+
+        while scroll <= post_number:
+            if last_post == 0 and scroll == 0:
+                divs_per_load = 0
+                time.sleep(3)
+            else:
+                divs_per_load = 4
+            if scroll >= last_post - 12:
+                image_row = image_container.find_elements(By.CLASS_NAME, value="_ac7v")
+                for row in image_row[-divs_per_load:]:
+                    time.sleep(0.2)
+                    anchor_tags = row.find_elements(By.TAG_NAME, value='a')
+                    for anchor in anchor_tags:
+                        href = anchor.get_attribute('href')
+                        image_links.append(href)
+            html.send_keys(Keys.END)
+
+            scroll += 12
+            time.sleep(2.5)
+
+        try:
+            with open(f'downloads/{self.account}/{self.account}_info_posts.txt', mode='w') as file:
+                file.write(str(scroll))
+        except FileNotFoundError:
+            os.mkdir(f'downloads/{self.account}')
+            with open(f'downloads/{self.account}/{self.account}_info_posts.txt', mode='w') as file:
+                file.write(str(scroll))
+
+        return image_links
+
+    def get_img_src(self, links):
+        images_links = []
+
+        for link in links:
+            self.driver.get(link)
+            #Ensure that get only images post
+            time.sleep(1.93)
+            try:
+                div_image = self.driver.find_element(By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div[2]/div[1]/article/div/div[1]/div/div/div/div[1]/div[1]')
+
+                img_tag = div_image.find_element(By.TAG_NAME, value='img')
+
+            except selenium.common.exceptions.NoSuchElementException:
+                try:
+                    time.sleep(1)
+                    div_image = self.driver.find_element(By.XPATH,
+                    value='/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div/div[1]/div/div[1]/div/div/div/div/div[1]/div/div[1]/div[2]/div/div/div/ul/li[2]/div/div/div/div/div[1]/div[1]')
+
+                    img_tag = div_image.find_element(By.TAG_NAME, value='img')
+
+                except selenium.common.exceptions.NoSuchElementException:
+                    print("No image post! pass")
+                    pass
+                else:
+                    images_links.append(img_tag.get_attribute('src'))
+
+            else:
+                images_links.append(img_tag.get_attribute('src'))
+
+        return images_links
+
+
+
+    def download_images(self, image_links):
+
+        try:
+            os.mkdir(f"downloads/{self.account}/posts")
+
+        except FileExistsError:
+            pass
+
+        try:
+            with open(f'downloads/{self.account}/{self.account}_info_posts.txt', mode='r') as post_info:
+                image_number = int(post_info.read())
+        except FileNotFoundError:
+                image_number = 1
+
+        for link in image_links:
+            self.driver.get(link)
+            response = requests.get(link)
+
+            with open(f'downloads/{self.account}/posts/{image_number}.jpg', mode='wb') as file:
+                file.write(response.content)
+                print('saving image')
+                image_number += 1
+                time.sleep(1.5)
+
+
+        self.driver.quit()
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-browser = Browser(target_account='puppys.heaven')
+browser = Browser(target_account='kodox.m4c')
 browser.login()
 browser.load_target_account()
+image_links = browser.get_img_src(links=browser.load_image_links())
+if image_links:
+    browser.download_images(image_links)
